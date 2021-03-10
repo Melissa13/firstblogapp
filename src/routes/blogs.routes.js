c1e3bd141ddf487915b20409cccadb6a573fa994
melissalf <melissalantiguafermin@gmail.com>
2021-03-10 11:29:15 -0400

    simplify routes files a little

--- src/routes/blogs.routes.js
+++ src/routes/blogs.routes.js
@@ -1,22 +1,9 @@
+const { createBasicRoutesFor } = require('./basicroutes');
+
 module.exports = (app) => {
   const blogs = require('../controllers/blogs.controller');
 
-  const router = require('express').Router();
-
-  // Create a new User
-  router.post('/', blogs.createEntry);
-
-  // Retrieve all blogs
-  router.get('/', blogs.findAllEntries);
-
-  // Retrieve a single user with id
-  router.get('/:id', blogs.findOneEntry);
-
-  // Update a blogs with id
-  router.put('/:id', blogs.updateEntry);
-
-  // Delete a blogs with id
-  router.delete('/:id', blogs.deleteEntry);
+  const router = createBasicRoutesFor(blogs);
 
   app.use('/api/blogs', router);
 };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,22 +1,9 @@
+const { createBasicRoutesFor } = require('./basicroutes');
+
 module.exports = (app) => {
   const users = require('../controllers/users.controller');
 
-  const router = require('express').Router();
-
-  // Create a new User
-  router.post('/', users.createEntry);
-
-  // Retrieve all Users
-  router.get('/', users.findAllEntries);
-
-  // Retrieve a single user with id
-  router.get('/:id', users.findOneEntry);
-
-  // Update a Users with id
-  router.put('/:id', users.updateEntry);
-
-  // Delete a Users with id
-  router.delete('/:id', users.deleteEntry);
+  const router = createBasicRoutesFor(users);
 
   app.use('/api/users', router);
 };
