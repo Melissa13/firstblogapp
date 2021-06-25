1c69620b76ec03d9a0329400225d7dcef38a057f
melissa13 <melissalantigua@hotmail.com>
2021-06-25 07:35:26 -0400

    add eslint, prettier and airbnb

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Button } from 'antd';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>react logotype makes me think on nuclear bombs...</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button type="primary">Button from ant design</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
